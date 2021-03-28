1b3c9caa368f2349566d27b2c06f449db6cef8b7
georgeboghez <35366691+georgeboghez@users.noreply.github.com>
2021-03-28 20:16:16 +0300

    login backend captcha validation

--- routes/user.js
+++ routes/user.js
@@ -47,156 +47,166 @@ router.post(
                 errorSignup: true
             });
         }
-        if (
-            req.body['g-recaptcha-response'] === undefined ||
-            req.body['g-recaptcha-response'] === '' ||
-            req.body['g-recaptcha-response'] === null
-            ) {
+        if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+            return res.status(400).render("./login-signup.html", {
+                errors: ['Captcha not completed'],
+                errorSignup: true
+            })
+        }
+        var secretKey = CONSTANTS.CAPTCHA_KEY
+        var verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}`
+        request(verificationUrl, function (error, response, body) {
+            body = JSON.parse(body)
+            if (body.success !== undefined && !body.success) {
+                
                 return res.status(400).render("./login-signup.html", {
-                    errors: ['Captcha not completed'],
+                    errors: ['Failed verification!'],
                     errorSignup: true
                 })
             }
-            console.log(CONSTANTS.CAPTCHA_KEY)
-            var secretKey = CONSTANTS.CAPTCHA_KEY
-            var verificationUrl =
-            'https://www.google.com/recaptcha/api/siteverify?secret=' +
-            secretKey +
-            '&response=' +
-            req.body['g-recaptcha-response']
-            console.log("ceva")
-            request(verificationUrl, function (error, response, body) {
-                body = JSON.parse(body)
-                if (body.success !== undefined && !body.success) {
-                    
-                    return res.status(400).render("./login-signup.html", {
-                        errors: ['Failed verification!'],
-                        errorSignup: true
-                    })
-                }
-                else {
-                    
-                }
-            })
-            const {
-                username,
-                email,
-                password
-            } = req.body;
-            try {
-                const query = datastore.createQuery('User').filter("email", "=", email);
-                const [users] = await datastore.runQuery(query);
+            else {
                 
-                if (users.length > 0) {
+            }
+        })
+        const {
+            username,
+            email,
+            password
+        } = req.body;
+        try {
+            const query = datastore.createQuery('User').filter("email", "=", email);
+            const [users] = await datastore.runQuery(query);
+            
+            if (users.length > 0) {
+                return res.status(400).render("./login-signup.html", {
+                    errors: ["User Already Exists"],
+                    errorSignup: true
+                });
+            }
+            
+            const kind = "User";
+            const userKey = datastore.key([kind]);
+            user = {
+                key: userKey,
+                data: {
+                    "username": username,
+                    "email" : email,
+                    "password": password
+                }
+            };
+            
+            const salt = await bcrypt.genSalt(10);
+            user.data.password = await bcrypt.hash(password, salt);
+            
+            // Saves the entity
+            await datastore.save(user);
+            
+            const payload = {
+                user: {
+                    id: user.userKey
+                }
+            };
+            jwt.sign(
+                payload,
+                "secret", {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.cookie("Authorization", token)
+                    res.cookie("Email", email)
+                    setTimeout(() => {
+                        res.redirect("/")                            
+                    }, 3000);
+                }
+                );
+            } catch (err) {
+                console.log(err.message);
+                res.status(500).send("Error in Saving");
+            }
+        }
+        );
+        router.post(
+            "/login",
+            [
+                check("email", "Please enter a valid email").isEmail(),
+                check("password", "Please enter a valid password").isLength({
+                    min: 6
+                })
+            ],
+            async (req, res) => {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
                     return res.status(400).render("./login-signup.html", {
-                        errors: ["User Already Exists"],
-                        errorSignup: true
+                        errors: errors.array().map((err) => {
+                            return err.msg;
+                        })
                     });
                 }
                 
-                const kind = "User";
-                const userKey = datastore.key([kind]);
-                user = {
-                    key: userKey,
-                    data: {
-                        "username": username,
-                        "email" : email,
-                        "password": password
-                    }
-                };
-                
-                const salt = await bcrypt.genSalt(10);
-                user.data.password = await bcrypt.hash(password, salt);
-                
-                // Saves the entity
-                await datastore.save(user);
-                
-                const payload = {
-                    user: {
-                        id: user.userKey
-                    }
-                };
-                jwt.sign(
-                    payload,
-                    "secret", {
-                        expiresIn: 3600
-                    },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.cookie("Authorization", token)
-                        res.cookie("Email", email)
-                        setTimeout(() => {
-                            res.redirect("/")                            
-                        }, 3000);
-                    }
-                    );
-                } catch (err) {
-                    console.log(err.message);
-                    res.status(500).send("Error in Saving");
-                }
-            }
-            );
-            router.post(
-                "/login",
-                [
-                    check("email", "Please enter a valid email").isEmail(),
-                    check("password", "Please enter a valid password").isLength({
-                        min: 6
+                if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+                    return res.status(400).render("./login-signup.html", {
+                        errors: ['Captcha not completed']
                     })
-                ],
-                async (req, res) => {
-                    const errors = validationResult(req);
-                    if (!errors.isEmpty()) {
-                        return res.status(400).render("./login-signup.html", {
-                            errors: errors.array().map((err) => {
-                                return err.msg;
-                            })
-                        });
-                    }
-                    const {
-                        email,
-                        password
-                    } = req.body;
-                    try {
-                        const query = datastore.createQuery('User').filter("email", "=", email);
+                }
+                var secretKey = CONSTANTS.CAPTCHA_KEY
+                var verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}`
+                request(verificationUrl, function (error, response, body) {
+                    body = JSON.parse(body)
+                    if (body.success !== undefined && !body.success) {
                         
-                        const [users] = await datastore.runQuery(query);
-                        
-                        if (users.length == 0) {
-                            return res.status(400).render("./login-signup.html", {
-                                errors: ["User does not exist"]
-                            });
-                        }
-                        let user = users[0]
-                        const isMatch = await bcrypt.compare(password, user.password);
-                        if (!isMatch)
                         return res.status(400).render("./login-signup.html", {
-                            errors: ["Incorrect Password"]
-                        });
-                        const payload = {
-                            user: {
-                                id: user.id
-                            }
-                        };
-                        jwt.sign(
-                            payload,
-                            "secret", {
-                                expiresIn: 3600
-                            },
-                            (err, token) => {
-                                if (err) throw err;
-                                res.cookie("Authorization", token)
-                                res.cookie("Email", email)
-                                res.redirect("/")
-                            }
-                            );
-                        } catch (e) {
-                            console.error(e);
-                            res.status(500).json({
-                                message: "Server Error"
-                            });
-                        }
+                            errors: ['Failed verification!']
+                        })
                     }
-                    );
+                    else {
+                        
+                    }
+                })
+                const {
+                    email,
+                    password
+                } = req.body;
+                try {
+                    const query = datastore.createQuery('User').filter("email", "=", email);
                     
-                    module.exports = router
+                    const [users] = await datastore.runQuery(query);
+                    
+                    if (users.length == 0) {
+                        return res.status(400).render("./login-signup.html", {
+                            errors: ["User does not exist"]
+                        });
+                    }
+                    let user = users[0]
+                    const isMatch = await bcrypt.compare(password, user.password);
+                    if (!isMatch)
+                    return res.status(400).render("./login-signup.html", {
+                        errors: ["Incorrect Password"]
+                    });
+                    const payload = {
+                        user: {
+                            id: user.id
+                        }
+                    };
+                    jwt.sign(
+                        payload,
+                        "secret", {
+                            expiresIn: 3600
+                        },
+                        (err, token) => {
+                            if (err) throw err;
+                            res.cookie("Authorization", token)
+                            res.cookie("Email", email)
+                            res.redirect("/")
+                        }
+                        );
+                    } catch (e) {
+                        console.error(e);
+                        res.status(500).json({
+                            message: "Server Error"
+                        });
+                    }
+                }
+                );
+                
+                module.exports = router
